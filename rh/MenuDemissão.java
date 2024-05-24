3032383dc1626ff5ea37e5ef6d218d8b2bb874c0
Caio Zancheta Selpa <145915550+Caio554@users.noreply.github.com>
2024-05-23 22:25:15 -0300

    Finalizado
    Ainda precisa ajustar algumas coisas, mas a grosso modo, esta pronto :D

---
+++ rh/MenuDemissão.java
@@ -0,0 +1,47 @@
+package rh; 
+import java.util.Scanner;
+
+public class MenuDemissão{
+    public static void MenuDE() {
+       
+        System.out.println("                                         ");
+        System.out.println("█████████████████████████████████████████");
+        System.out.println("█                                       █");
+        System.out.println("█             !Funcionarios!            █");
+        System.out.println("█                                       █");
+        System.out.println("█     1. Demitir                        █");
+        System.out.println("█     2. Lista de demissões             █");
+        System.out.println("█     3. readmitir funcionario          █");
+        System.out.println("█     4. Lista de funcionarios          █");
+        System.out.println("█     5. Voltar                         █");
+        System.out.println("█                                       █");
+        System.out.println("█             !Funcionarios!            █");
+        System.out.println("█                                       █");
+        System.out.println("█████████████████████████████████████████");
+        System.out.println("                                        ");  
+
+        String MenuDeDemissão = lerDados.lerTexto("Tente novamente");
+
+        switch (MenuDeDemissão) {
+            case "1":
+            RHSystemUtils.demitirfuncionario();
+            break;
+            case "2":
+            RHSystemUtils.readmitirfuncionario();
+            break;
+            case "3":
+            RHSystemUtils.readmitirfuncionario();
+            break;
+            case "4":
+            RHSystemUtils.listarFuncionarios();
+            break;
+            case "5":
+            RHSystem.menu();
+            break;
+            default:
+            System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+            break;  
+        }
+    }
+
+}
---
+++ rh/MenuFuncionario.java
@@ -0,0 +1,49 @@
+package rh;
+import java.util.Scanner;
+
+
+public class MenuFuncionario { 
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void Employee() {
+        System.out.println("                                         ");
+        System.out.println("█████████████████████████████████████████");
+        System.out.println("█                                       █");
+        System.out.println("█             !Funcionarios!            █");
+        System.out.println("█                                       █");
+        System.out.println("█     1. Admitir Funcionário            █");
+        System.out.println("█     2. Listar funcionários            █");
+        System.out.println("█     3. Pedir ferias                   █");
+        System.out.println("█     4. Voltar de ferias               █");
+        System.out.println("█     5. Voltar                         █");
+        System.out.println("█                                       █");
+        System.out.println("█             !Funcionarios!            █");
+        System.out.println("█                                       █");
+        System.out.println("█████████████████████████████████████████");
+        System.out.println("                                       ");  
+
+        
+        String menuFuncionario  = lerDados.lerTexto("Tente novamente");
+
+        switch (menuFuncionario){
+            case "1":
+            RHSystemUtils.adicionarFuncionario();
+            break;
+            case "2":
+            RHSystemUtils.listarFuncionarios();
+            break;
+            case "3":
+            RHSystemUtils.solicitarFerias();
+            break;
+            case "4":
+            RHSystemUtils.retornarAoTrabalho();
+            break;
+            case "5": 
+            RHSystem.menu();
+            break;
+            default:
+            System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+            break;  
+        }
+    }
+}
